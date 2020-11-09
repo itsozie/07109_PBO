b373c928cc1e461568b51a50a0a7029354bdecfa
ozie77 <72397231+itsozie@users.noreply.github.com>
2020-11-09 21:48:08 +1000

    Add files via upload

---
+++ praktikum/NPM07109PraktikumModul1/src/ManajemenLaundry/fatchur_07109_OwnerEntity.java
@@ -0,0 +1,56 @@
+package ManajemenLaundry;
+import java.util.Date;
+public class fatchur_07109_OwnerEntity {
+        String Fatchur_07109_nama_pelanggan;
+        Date Fatchur_07109_tanggal_masuk;
+        String Fatchur_07109_antrian;
+        int Fatchur_07109_indexpaket;
+        String Fatchur_07109_pegawai;
+        float Fatchur_07109_berat_cucian;
+        Date Fatchur_07109_tanggal_selesai;
+    
+        
+        public fatchur_07109_OwnerEntity(
+                String Fatchur_07109_nama_pelanggan,
+                Date Fatchur_07109_tanggal_masuk,
+                String Fatchur_07109_antrian,
+                int Fatchur_07109_paket,
+                String Fatchur_07109_pegawai,
+                float Fatchur_07109_berat_cucian, 
+                Date Fatchur_07109_tanggal_selesai){                              
+         this.Fatchur_07109_nama_pelanggan = Fatchur_07109_nama_pelanggan;
+         this.Fatchur_07109_tanggal_masuk = Fatchur_07109_tanggal_masuk;
+         this.Fatchur_07109_antrian = Fatchur_07109_antrian;
+         this.Fatchur_07109_indexpaket = Fatchur_07109_paket;
+         this.Fatchur_07109_pegawai = Fatchur_07109_pegawai;
+         this.Fatchur_07109_berat_cucian = Fatchur_07109_berat_cucian;
+         this.Fatchur_07109_tanggal_selesai = Fatchur_07109_tanggal_selesai;
+        }
+    public String getFatchur_07109_nama_pelanggan() {
+        return Fatchur_07109_nama_pelanggan;
+    }
+
+    public Date getFatchur_07109_tanggal_masuk() {
+        return Fatchur_07109_tanggal_masuk;
+    }
+
+    public String getFatchur_07109_antrian() {
+        return Fatchur_07109_antrian;
+    }
+
+    public int getFatchur_07109_paket() {
+        return Fatchur_07109_indexpaket;
+    }
+
+    public String getFatchur_07109_pegawai() {
+        return Fatchur_07109_pegawai;
+    }
+
+    public float getFatchur_07109_berat_cucian() {
+        return Fatchur_07109_berat_cucian;
+    }
+
+    public Date getFatchur_07109_tanggal_selesai() {
+        return Fatchur_07109_tanggal_selesai;
+    }
+}
