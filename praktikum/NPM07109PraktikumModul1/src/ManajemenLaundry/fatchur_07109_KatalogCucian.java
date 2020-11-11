aee66ac294d0a0b17c6e7a3079b0278f85614822
ozie77 <72397231+itsozie@users.noreply.github.com>
2020-11-11 20:45:58 +1000

    delete folder modul

--- praktikum/NPM07109PraktikumModul1/src/ManajemenLaundry/fatchur_07109_KatalogCucian.java
+++
@@ -1,4 +0,0 @@
-package ManajemenLaundry;
-public class fatchur_07109_KatalogCucian {
-     static String [] katalog ={"Cuci Kering","Cuci Setrika","Cuci Expres"}; 
-}
--- praktikum/NPM07109SoalPendahuluan1/src/com/NPM07109SoalPendahuluan1/PraktikumEntity.java
+++
@@ -1,5 +0,0 @@
-package com.NPM07109SoalPendahuluan1;
-
-public class PraktikumEntity {
-    static String [] Praktikum = {"PBO", "SO", "PETRUK"};
-}
